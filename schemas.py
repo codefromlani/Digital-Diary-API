fbff3632da2693ba0b9587d1a421e9acc5b3eaf4
Rodiat Hammed <rodiathammed48@gmail.com>
2024-11-25 11:23:47 +0100

    added token

--- schemas.py
+++ schemas.py
@@ -62,3 +62,11 @@ class DiaryEntryResponse(DiaryEntryBase):
 
     class Config:  
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
