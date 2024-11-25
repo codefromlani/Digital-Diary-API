396be6223ebf173ed5f7bd20b92e960eb92cfb92
Rodiat Hammed <rodiathammed48@gmail.com>
2024-11-25 11:30:06 +0100

    authentication utilities

---
+++ auth.py
@@ -0,0 +1,58 @@
+from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status
+import os
+from dotenv import load_dotenv
+from database import get_db
+from sqlalchemy.orm import Session
+import models
+import schemas
+from typing import Optional
+from datetime import timedelta, datetime
+from jose import jwt, JWTError
+
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def authenticate_user(db: Session, username: str, password: str):
+    """Authenticate a user"""
+    user = db.query(models.User).filter(models.User.username == username).first()
+    if not user or not models.verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    """Create JWT access token"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> models.User:
+    """Get current user from token"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    
+    user = db.query(models.User).filter(models.User.username == token_data.username).first()
+    if user is None:
+        raise credentials_exception
+    return user
