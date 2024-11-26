371d37492252ebaa71abde7d65e3cc8de4bbeabb
Rodiat Hammed <rodiathammed48@gmail.com>
2024-11-26 12:43:02 +0100

    added dependency

--- database.py
+++ database.py
@@ -6,16 +6,16 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 Sessionlocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base
+Base = declarative_base()
 
 def get_db():
-    db = Sessionlocal
+    db = Sessionlocal()
     try:
         yield db
     finally:
